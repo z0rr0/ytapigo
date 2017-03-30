b48fe9d22c457cc20989a4d1485327f00a2ee22f
Alexander Zaytsev <thebestzorro@yandex.ru>
2017-03-30 22:51:48 +0300

    added version.sh

---
+++ version.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ${GOPATH}/src/github.com/z0rr0/ytapigo
+
+FILENAME="version"
+TS="`TZ=UTC date +\"%F_%T\"`UTC"
+TAG="`git tag | sort --version-sort | tail -1`"
+VER="`git log --oneline | head -1 `"
+
+if [[ -z "$TAG" ]]; then
+    TAG="N/A"
+fi
+
+echo "-X main.Version=${TAG} -X main.Revision=git:${VER:0:7} -X main.Date=${TS}"
